44a43e3c3935cd9a29e7c79c982870c81b97611d
Melnard <dejesusmelnard@gmail.com>
2024-06-30 08:57:48 +0800

    paginate

---
+++ src/Data/data..ts
@@ -0,0 +1,85 @@
+interface Item {
+  name: string;
+  country: string;
+  classification: string;
+}
+
+const data: Item[] = [
+  {
+    name: "World Bank Data",
+    country: "United States",
+    classification: "International Development",
+  },
+  {
+    name: "European Central Bank Data",
+    country: "European Union",
+    classification: "Financial/Economic",
+  },
+  {
+    name: "UNESCO Institute for Statistics",
+    country: "France",
+    classification: "Education/Culture",
+  },
+  {
+    name: "PubMed",
+    country: "United States",
+    classification: "Health/Medical",
+  },
+  {
+    name: "OECD Data",
+    country: "France",
+    classification: "Economic/Social",
+  },
+  {
+    name: "United Nations Data",
+    country: "United Nations",
+    classification: "Global Statistics",
+  },
+  {
+    name: "IMF Data",
+    country: "United States",
+    classification: "Financial/Economic",
+  },
+  {
+    name: "ICPSR",
+    country: "United States",
+    classification: "Social Science Research",
+  },
+  {
+    name: "FAOSTAT",
+    country: "Italy",
+    classification: "Agriculture/Food",
+  },
+  {
+    name: "National Center for Education Statistics (NCES)",
+    country: "United States",
+    classification: "Education",
+  },
+  {
+    name: "World Health Organization (WHO)",
+    country: "Switzerland",
+    classification: "Health",
+  },
+  {
+    name: "Census of India",
+    country: "India",
+    classification: "Demographic",
+  },
+  {
+    name: "Statistics Canada",
+    country: "Canada",
+    classification: "Demographic/Economic",
+  },
+  {
+    name: "Australian Bureau of Statistics",
+    country: "Australia",
+    classification: "Demographic/Economic",
+  },
+  {
+    name: "Eurostat",
+    country: "European Union",
+    classification: "Economic/Demographic",
+  },
+];
+
+export default data;
